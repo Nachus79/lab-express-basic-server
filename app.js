fb76d0dafb871d590c0db6d50ce2e59f25822ad0
Nacho Monzón <ignaciomonzona@gmail.com>
2025-01-24 20:27:29 +0100

    Iteration 5 complete

--- app.js
+++ app.js
@@ -31,14 +31,17 @@ app.get('/blog', (req, res) => { //Iteración 4
     res.sendFile(__dirname + '/views/blog.html');
 }); 
 
+app.get('/api/projects', (req, res) => {
 
+    res.sendFile(__dirname + '/data/projects.json'); 
+});
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
 
 app.listen(5005, () => console.info("Server listening at port 5005"));
 
-
+/*
 Create a route GET /api/projects that responds back with the JSON data from the file data/projects.json.
 The purpose of this route is to provide the JSON data for the home.html page, which requests and renders it in the My Projects section as a list.
 
@@ -48,7 +51,7 @@ You will need to import the file data/projects.json in app.js.
 To test the route, navigate to localhost:5005/api/projects in your browser.
 If you set up the route correctly, you should see the JSON data with projects sent in the response.
 
-
+*/
 
 
 
